a3db3e135c63014733e609d9aa12f106dcd4894d
anpuqiang <puqianga@opera.com>
2020-04-12 20:46:31 +0800

    add comment

--- main/main.go
+++ main/main.go
@@ -1,8 +1,8 @@
 package main
 
-import ""
+import "stockDataCollect/service"
 
 //数据API来源：阿里云 易源数据-股票历史行情查询_免费版
 func main() {
-	service.GetStockData("")
+	service.GetStockData("2020-04-01", "2020-04-02", "600004")
 }
--- service/stock.go
+++ service/stock.go
@@ -8,6 +8,49 @@ import (
 )
 
 //begin=2015-09-01&code=600004&end=2015-09-02
+/** response format
+{
+    "showapi_res_error":"",
+    "showapi_res_id":"f3b49de402564d79a30c415c31c6b8e0",
+    "showapi_res_code":0,
+    "showapi_res_body":{
+        "ret_code":0,
+        "showapi_fee_code":-1,
+        "list":[
+            {
+                "trade_money":"135201992", //交易金额
+                "diff_money":"0.20",	//涨跌金额
+                "code":"600004",		//股票代码
+                "open_price":"12.46",	//开盘价
+                "date":"2020-04-02",	//日期
+                "market":"sh",			//市场
+                "min_price":"12.27",	//最低价
+                "trade_num":"108310",	//交易手数
+                "turnover":"0.52",		//换手率
+                "close_price":"12.65",	//收盘价
+                "max_price":"12.67",	//最高价
+                "swing":"3.21",			//振幅
+                "diff_rate":"1.61"		//涨跌幅(%)
+            },
+            {
+                "trade_money":"105467835",
+                "diff_money":"-0.10",
+                "code":"600004",
+                "open_price":"12.52",
+                "date":"2020-04-01",
+                "market":"sh",
+                "min_price":"12.45",
+                "trade_num":"83741",
+                "turnover":"0.4",
+                "close_price":"12.45",
+                "max_price":"12.81",
+                "swing":"2.87",
+                "diff_rate":"-0.8"
+            }
+        ]
+    }
+}
+*/
 func GetStockData(begin string, end string, code string) {
 	host := "http://stock.market.alicloudapi.com"
 	path := "/sz-sh-stock-history"
